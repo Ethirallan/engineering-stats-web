95e5cf328a6ed48b208a113e26940e3af1476dd4
Ethirallan <benjamin.bozic@ethirallan.com>
2025-11-10 11:33:40 +0100

    feat: update router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,14 @@
 import { Routes } from '@angular/router';
 import { NotFoundPageComponent } from './shared/pages/not-found-page/not-found-page.component';
+import { TeamsDetailsPageComponent } from './teams/pages/teams-details-page/teams-details-page.component';
+import { TeamsListPageComponent } from './teams/pages/teams-list-page/teams-list-page.component';
+import { TeamsStatsPageComponent } from './teams/pages/teams-stats-page/teams-stats-page.component';
+import { EngineersStatsComponent } from './engineers/pages/engineers-stats/engineers-stats.component';
 
 export const routes: Routes = [
+  { path: '', component: TeamsListPageComponent },
+  { path: 'details/:id', component: TeamsDetailsPageComponent },
+  { path: 'stats/:id', component: TeamsStatsPageComponent },
+  { path: 'engineers/:id/stats', component: EngineersStatsComponent },
   { path: '**', component: NotFoundPageComponent },
 ];
