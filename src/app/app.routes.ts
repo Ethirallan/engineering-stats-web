784653ee14952dfdb93a2723e6a4ed290fbf0f10
Ethirallan <benjamin.bozic@ethirallan.com>
2025-11-10 11:33:39 +0100

    feat: add 404 not found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { NotFoundPageComponent } from './shared/pages/not-found-page/not-found-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '**', component: NotFoundPageComponent },
+];
---
+++ src/app/shared/pages/not-found-page/not-found-page.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-not-found-page',
+  imports: [],
+  templateUrl: './not-found-page.component.html',
+  styleUrl: './not-found-page.component.scss',
+})
+export class NotFoundPageComponent {
+  constructor(private router: Router) {}
+
+  navigateToHome() {
+    this.router.navigate(['/']);
+  }
+}
