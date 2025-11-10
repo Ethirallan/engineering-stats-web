958b6411740230539268243aba1fe6686e415fff
Ethirallan <benjamin.bozic@ethirallan.com>
2025-11-10 11:33:39 +0100

    test(unit): extend tests for app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,48 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { provideRouter } from '@angular/router';
+import { Component } from '@angular/core';
+
+@Component({
+  template: '<h1>Home Page</h1>'
+})
+class MockHome { }
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [
+        provideRouter([
+          { path: '', component: MockHome }
+        ])
+      ]
     }).compileComponents();
   });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'engineering-stats-web' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('engineering-stats-web');
-  });
-
-  it('should render title', () => {
+  it('should render navigation links', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, engineering-stats-web');
+    const navLinks = compiled.querySelectorAll('nav a');
+
+    expect(navLinks.length).toBe(2);
+    expect(navLinks[0].textContent).toContain('Engineering Benchmarks');
+    expect(navLinks[1].textContent).toContain('Teams');
+  });
+
+  it('should contain a router outlet', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    expect(compiled.querySelector('router-outlet')).not.toBeNull();
   });
 });
